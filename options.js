c9455e5efdd61e9de7e7a59e22c31d97a2361b95
e <julian.handl@bestit-online.at>
2018-02-02 12:36:12 +0100

    working on rendering

--- options.js
+++ options.js
@@ -1,9 +1,13 @@
 // Saves options to chrome.storage
 function save_options() {
     var timeformat = document.getElementById('timeformat').value;
+    var minPause = document.getElementById('forced-pause').value;
+    var minPauseHours = document.getElementById('forced-pause-hours').value;
 
     chrome.storage.sync.set({
-        timeformat: timeformat
+        timeformat: timeformat,
+        minPause: minPause,
+        minPauseHours: minPauseHours
     }, function () {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -17,9 +21,13 @@ function save_options() {
 function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
-        timeformat: 'decimal'
+        timeformat: 'decimal',
+        minPause: '0',
+        minPauseHours: '6'
     }, function (items) {
         document.getElementById('timeformat').value = items.timeformat;
+        document.getElementById('forced-pause').value = items.minPause;
+        document.getElementById('forced-pause-hours').value = items.minPauseHours;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
--- timetrackings.js
+++ timetrackings.js
@@ -1,21 +1,29 @@
+// TODO: Save Abwesendheiten in Cookie and Consider in timetracking
+
 // get settings
 chrome.storage.sync.get({
-    timeformat: 'decimal'
+    timeformat: 'decimal',
+    minPause: '0',
+    minPauseHours: undefined
 }, function (items) {
-    var timeformat = items.timeformat;
+    var settings = {
+        timeformat: items.timeformat,
+        minPause: items.minPause,
+        minPauseHours: items.minPauseHours
+    };
 
     // check with an interval if the table is rendered
     var checkInterval = setInterval(function(){
         var currentTable = document.getElementById("time_trackings_list");
         if(currentTable){
             clearInterval(checkInterval);
-            parse(currentTable, timeformat);
+            parse(currentTable, settings);
 
             // observe table changes to react on table filtering
             var observer = new MutationObserver(function(){
                 var newTable = document.getElementById("time_trackings_list");
                 if(currentTable !== newTable){
-                    parse(newTable, timeformat);
+                    parse(newTable, settings);
                     currentTable = newTable;
                 }
             });
@@ -27,7 +35,7 @@ chrome.storage.sync.get({
 
 var weekdays = ['Mo','Di','Mi','Do','Fr','Sa','So'];
 
-function parse(table, timeformat){
+function parse(table, settings){
     var rows = table.getElementsByClassName("entry");
     var weeks = [];
     var totalHoursInWeek = 0;
@@ -88,6 +96,10 @@ function parse(table, timeformat){
             var pauseColumn = row.getElementsByClassName('pause')[0];
             var pauseString = pauseColumn.innerHTML.trim();
             var pauseValue = null;
+            var pause = {
+                value: 0,
+                items: []
+            };
             if(entryIsRunning){
                 var hasUnregisteredPause = pauseColumn.getElementsByTagName('span').length > 0;
                 if(!hasUnregisteredPause){
@@ -106,7 +118,7 @@ function parse(table, timeformat){
                 if(pauseDetailedString){
                     pauseDetails = pauseDetailedString.split('<br>');
                 }
-                var pause = {
+                pause = {
                     value: parseFloat(pauseValueString),
                     items: []
                 };
@@ -124,12 +136,15 @@ function parse(table, timeformat){
                     }
                 });
 
-                // TODO: Make this a setting: If more than 6h pause must be >= 0.5h
-                if(pause.value < 0.5 && day.bruttoTime.value > 6.0){
-                    pause.value = 0.5;
+                if(
+                    settings.minPauseHours &&
+                    day.bruttoTime.value > parseFloat(setting.minPauseHours &&
+                        pause.value < parseFloat(settings.minPause)
+                )){
+                    pause.value = parseFloat(settings.minPause);
                 }
             }
-
+            day.pause = pause;
 
             // start new week
             if(i === rows.length-1){
@@ -149,140 +164,22 @@ function parse(table, timeformat){
 
     console.log("Weeks", weeks);
 
-    if(rows){
-        for(var i=0; i<rows.length; i++){
-            var row = rows[i];
+    renderIntoTable(weeks); 
+}
 
-            // get columns of netto hours
-            var nettoHoursColumn = row.getElementsByClassName('netto netto_time')[0];
-            // get value of netto hours
-            var nettoHours = parseFloat(nettoHoursColumn.innerHTML.split(' ')[0]);
+function renderIntoTable(weeks){
+    // delete included overview lines
 
-            // check if day is still running
-            var timeColumn = row.getElementsByClassName("end_start_time")[0];
-            var entryIsRunning = timeColumn.innerHTML.indexOf("Seit") >= 0; 
-
-            // if day is still running
-            if(entryIsRunning){
-                // get pause column
-                var pauseIndicator = row.querySelectorAll('[data-detailed-pauses]');
-                var pauseString = pauseIndicator && pauseIndicator.length > 0 ? pauseIndicator[0].getAttribute("data-detailed-pauses") : null;
-                
-                // if pause column
-                if(pauseString){
-                    var pauseCol = row.getElementsByClassName('pause')[0];
-
-                    // parse pause data
-                    var pauseArray = pauseString.split("<br>").map(function(p){
-                        var pauseData = pauseString.split(" - ");
-
-                        // parse pause start
-                        var pauseStart = new Date();
-                        pauseStart.setHours(pauseData[0].split(':')[0]);
-                        pauseStart.setMinutes(pauseData[0].split(':')[1]);
-
-                        // parse pause end
-                        var pauseEnd = new Date();
-                        pauseEnd.setHours(pauseData[1].split(':')[0]);
-                        pauseEnd.setMinutes(pauseData[1].split(':')[1]);
-                        return (pauseEnd - pauseStart) / 1000 / 60 / 60;
-                    });
-
-                    // sum all pauses
-                    var pauseLength = 0;
-                    pauseArray.forEach(function(p){
-                        pauseLength += p;
-                    });
-
-                    // if you work longer than 6h you have to take 0.5h pause
-                    if(nettoHours > 6.0 && pauseLength < 0.5){
-                        pauseLength = 0.5;
-                    }
-
-                    // calculate new netto hours and set pause
-                    nettoHours = (Math.round((nettoHours - pauseLength) * 100) / 100);
-
-                    // if timeformat is hour, convert and set html
-                    if(timeformat === "hour"){
-                        nettoHoursColumn.innerHTML = decimalToHour(nettoHours) + " h";
-                        pauseCol.innerHTML = decimalToHour(pauseLength) + " h " + (pauseCol.innerHTML.substring(pauseCol.innerHTML.indexOf('<span')));
-                    }
-                    else{
-                        nettoHoursColumn.innerHTML = nettoHours + " h";
-                        pauseCol.innerHTML = (Math.round((pauseLength) *100) / 100) + " h " + (pauseCol.innerHTML.substring(pauseCol.innerHTML.indexOf('<span')));
-                    }
-                }
-            }
-            else {
-                // if the row is not a currently running entry
-
-                // if timeformat is hour
-                if(timeformat === "hour"){
-                    // convert the netto column to hour format
-                    var nettoColumn = row.getElementsByClassName('netto')[0];
-                    var nettoTime = parseFloat(nettoColumn.innerHTML.split(' ')[0]);
-                    nettoColumn.innerHTML = decimalToHour(nettoTime) + ' h';
-
-                    // convert the pause column to hour format
-                    var pauseColumn = row.getElementsByClassName('pause')[0];
-                    var pauseSpanIndex = pauseColumn.innerHTML.indexOf('<span');
-                    var pauseTimeString = pauseColumn.innerHTML.slice(0,pauseSpanIndex-1).trim();
-                    var pauseTime = parseFloat(pauseTimeString.split(' ')[0]);
-                    pauseColumn.innerHTML = decimalToHour(pauseTime) + ' h ' + pauseColumn.innerHTML.substring(pauseSpanIndex);
-
-                }
-            }
-
-            // convert brutto column to hour if timeformat is hour
-            if(timeformat === "hour"){
-                var bruttoColumn = row.getElementsByClassName('brutto')[0];
-                var bruttoTime = parseFloat(bruttoColumn.innerHTML.split(' ')[0]);
-                bruttoColumn.innerHTML = decimalToHour(bruttoTime) + ' h';
-            }
-
-            // add nettoHours to total week hours
-            totalHoursInWeek += nettoHours;
-
-            // get current day
-            var dayColumnHTML = row.getElementsByClassName('day')[0].innerHTML;
-            var currentDay = dayColumnHTML.split(',')[0].slice(-2);
-
-            // if this is not the last row
-            if(i < rows.length - 1){
-                // get next day
-                var nextDayColumnHTML = rows[i+1].getElementsByClassName('day')[0].innerHTML;
-                var nextDay = nextDayColumnHTML.split(',')[0].slice(-2);
-                // check if this is the first day of a week
-                var newWeek = weekdays.indexOf(currentDay) < weekdays.indexOf(nextDay);
-
-                // if first day of a week
-                if(newWeek){
-                    // create new table row
-                    var totalString = (Math.round(totalHoursInWeek*100)/100);
-                    if(timeformat === "hour"){
-                        totalString = decimalToHour(totalHoursInWeek);
-                    }
-
-                    var newTR = document.createElement("tr");
-                    var newTD = document.createElement("td");
-                    newTD.innerHTML = '<strong>' + totalString + " h</strong>";
-                    newTR.appendChild(document.createElement("td"));
-                    newTR.appendChild(document.createElement("td"));
-                    newTR.appendChild(document.createElement("td"));
-                    newTR.appendChild(document.createElement("td"));
-                    newTR.appendChild(document.createElement("td"));
-                    newTR.appendChild(newTD);
-                    newTR.appendChild(document.createElement("td"));
-                    newTR.appendChild(document.createElement("td"));
-                    newTR.appendChild(document.createElement("td"));
-                    // insert table row with week total into table
-                    row.parentNode.insertBefore(newTR, row.nextSibling);
-                    // reset week total hours
-                    totalHoursInWeek = 0;
-                }
-            }
-        };
-    }
+    weeks.forEach(function(week){
+        week.forEach(function(day){
+            // Work on Zeiten
+            
+            // Work on Dauer (Brutto) 
+            // Work on Pause
+            // Work on Dauer (Netto)
+        });
+        // include overview line
+    });
 }
 
 // add zeros to the left
